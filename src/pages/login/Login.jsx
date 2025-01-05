002365eb1a33284564e0647c9418ee225ec4a383
Benjamin Sepulveda <benjaminsepulveda.0420@gmail.com>
2025-01-05 00:46:43 -0300

    login and register

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,46 +1,65 @@
 import axios from "axios";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
 import loginBg from "./img/login.jpg";
+import logo from "/src/img/logoSimple.png";
 
 export default function Login() {
-  let email = useRef();
-  let password = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const [error, setError] = useState(null);
 
   async function loginUser(e) {
     e.preventDefault();
-    let user = {
-      email: email.current.value,
-      password: password.current.value,
+    const user = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
     };
     try {
-      let { data } = await axios.post(
-        "https://minga-back-alpha.vercel.app/" + "auth/login",
+      const { data } = await axios.post(
+        "https://minga-back-alpha.vercel.app/auth/login",
         user
       );
-      console.log(data);
+      console.log("Login exitoso:", data);
     } catch (error) {
-      console.log(error);
+      if (error.response) {
+        setError(error.response.data.message || "Error de inicio de sesión");
+      } else if (error.request) {
+        setError("No se pudo conectar al servidor");
+      } else {
+        setError("Error desconocido");
+      }
+      console.error("Error de inicio de sesión:", error);
     }
   }
+
   return (
     <>
-      <div className="h-[100vh] bg-slate-800">
-        <img className="h-full object-cover" src={loginBg} alt="" />
+      <div className="bg-slate-800 w-full h-[100vh]">
+        <img className="w-full h-full object-cover" src={loginBg} alt="" />
       </div>
-      <div className="flex justify-center items-center w-1/2">
-        <form>
-          <div className="flex flex-col">
-            <label>Email</label>
-            <input type="email" ref={email} placeholder="example@email.com" />
-            <label>Password</label>
-            <input
-              type="password"
-              ref={password}
-              placeholder="Enter your password"
-            />
-            <button onClick={loginUser}>Login</button>
+      <div className="flex flex-col justify-center items-center w-full h-screen">
+        <div className="flex flex-col gap-4 mt-16">
+          <div className="flex justify-center items-center gap-2">
+            <p className="font-bold text-3xl text-orange-500">Minga</p>
+            <img className="w-8 h-8" src={logo} alt="" />
           </div>
-        </form>
+          <div className="flex justify-center items-center gap-2">
+            <p className="font-bold text-3xl text-black">Welcome</p>
+            <p className="font-bold text-3xl text-orange-500">back!</p>
+          </div>
+          <div className="flex justify-center items-center gap-2">
+          <p className="w-1/2 text-center text-gray-700">Discover manga, manhua and manhwa, track your progress, have fun, read manga.</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <input ref={emailRef} type="email" name="email" id="email" className="bg-white shadow-sm p-2 border rounded-md w-full h-10 placeholder-slate-400" placeholder="you@example.com" />
+            <input ref={passwordRef} type="password" name="password" id="password" className="bg-white shadow-sm p-2 border rounded-md w-full h-10 placeholder-slate-400" placeholder="Hola123456" />
+          </div>
+          {error && <p className="text-center text-red-500">{error}</p>}
+          <button onClick={loginUser} className="bg-gradient-to-t from-orange-500 to-orange-600 rounded-md w-full h-10 text-white">Sign In</button>
+          <Link to="/register" className="font-medium text-center text-orange-600 text-sm hover:underline">¿No tienes cuenta? Regístrate</Link>
+          <Link to="/" className="font-medium text-center text-orange-600 text-sm hover:underline">Volver a la página principal</Link>
+        </div>
       </div>
     </>
   );
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,50 +1,65 @@
-import axios from "axios";
-import { useRef } from "react";
+import { Link } from "react-router-dom";
 import registerImg from "./img/register1.jpg";
+import logo from "/src/img/logoSimple.png";
+import { useRef, useState } from "react";
+import axios from "axios";
+
+
 
 export default function Register() {
-  let username = useRef();
-  let email = useRef();
-  let password = useRef();
 
-  async function registerUser(e) {
-    e.preventDefault();
-    let newUser = {
-      user: username.current.value,
-      email: email.current.value,
-      password: password.current.value,
-    };
-    try {
-      let { data } = await axios.post(
-        "https://minga-back-alpha.vercel.app/" + "auth",
-        newUser
-      );
-      console.log(data);
-    } catch (error) {
-      console.log(error);
+    let username = useRef();
+    let email = useRef();
+    let password = useRef();
+  
+    const [error, setError] = useState(null);
+
+    async function registerUser(e) {
+      e.preventDefault();
+      let newUser = {
+        user: username.current.value,
+        email: email.current.value,
+        password: password.current.value,
+      };
+      try {
+        let { data } = await axios.post(
+          "https://minga-back-alpha.vercel.app/" + "auth",
+          newUser
+        );
+        console.log(data);
+      } catch (error) {
+        setError(error.response.data.message);
+      }
     }
-  }
-
   return (
     <>
-      <div className="flex flex-row justify-center items-center w-1/2">
-        <form className="w-[290px]">
-          <div className="flex flex-col">
-            <label>Username</label>
-            <input type="text" ref={username} placeholder="El Pepe" />
-            <label>Email</label>
-            <input type="email" ref={email} placeholder="example@mail.com" />
-            <label>Password</label>
-            <input
-              type="password"
-              ref={password}
-              placeholder="Enter your password"
-            />
-            <button onClick={registerUser}>Register</button>
+      <div className='w-full lg:w-1/2'>
+        <form onSubmit={registerUser} className='flex flex-col justify-center items-center gap-2 p-20 lg:p-40'>
+          <div className="flex justify-center items-center gap-2">
+            <p className="font-bold text-3xl text-orange-500">Minga</p>
+            <img className="w-8 h-8" src={logo} alt="Logo de Minga" />
           </div>
+          <div className="flex justify-center items-center">
+            <p className="font-bold text-3xl text-black">Welcome</p>
+            <p className="font-bold text-3xl text-orange-500">!</p>
+          </div>
+          <div className="flex justify-center items-center">
+            <p className="w-1/2 text-center text-gray-700">Descubre manga, manhua y manhwa, rastrea tu progreso, diviértete, lee manga.</p>
+          </div>
+          <input type="text" ref={username} className="bg-white shadow-sm p-2 border rounded-md w-full h-10 placeholder-slate-400" placeholder="Nombre de usuario" required />
+          <input type="email" ref={email} className="bg-white shadow-sm p-2 border rounded-md w-full h-10 placeholder-slate-400" placeholder="you@example.com" required />
+          <input type="password" ref={password} className="bg-white shadow-sm p-2 border rounded-md w-full h-10 placeholder-slate-400" placeholder="●●●●●●●●●●●●●" required />
+          <div className='flex items-center gap-2'>
+            <input type="checkbox" className='accent-orange-500' />
+            <p className="text-gray-700">Enviar notificaciones a mi correo</p>
+          </div>
+          {error && <p className="text-center text-red-500">{error}</p>}
+          <button onClick={registerUser} className="bg-gradient-to-t from-orange-500 to-orange-600 rounded-md w-full h-10 text-white">Sign up</button>
+          <p>Already have an account? <Link className="font-medium text-center text-orange-600 text-sm hover:underline" to={``}>Log in</Link></p>
+          <p>Go back to <Link className="font-medium text-center text-orange-600 text-sm hover:underline" to={`/`}>home page</Link></p>
         </form>
-      </div>
-      <div className="w-1/2 h-[100vh] bg-slate-800">
+        </div>
+      <div className="bg-slate-800 w-1/2 h-[100vh]">
         <img className="h-full object-cover" src={registerImg} alt="" />
       </div>
     </>
