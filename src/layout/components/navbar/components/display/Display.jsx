cbe25186e3a8b9bde5a8d07ca3bb39d0182c25b8
Benjamin Sepulveda <benjaminsepulveda.0420@gmail.com>
2025-01-04 23:50:43 -0300

    profile

--- src/layout/components/navbar/components/display/Display.jsx
+++ src/layout/components/navbar/components/display/Display.jsx
@@ -25,7 +25,7 @@ const HamburgerMenu = () => {
     },
     {
       name: "Profile",
-      link: "/profile/username",
+      link: "/profile/66fe32336aff4962dc74ea72",
     },
     {
       name: "Mangas",
@@ -39,10 +39,10 @@ const HamburgerMenu = () => {
         <img src={Menu} alt="Menu" />
       </button>
       {isOpen && (
-        <div className=" lg:w-3/12 w-full fixed bg-gradient-to-t from-orange-500 to-orange-600 h-screen  flex-col  top-0 left-0">
-          <div className="flex items-center p-4 justify-between">
-            <div className="flex gap-2 items-center cursor-pointer">
-              <img src={avatar} className="h-12 z-40" alt="" />
+        <div className="top-0 left-0 fixed flex-col bg-gradient-to-t from-orange-500 to-orange-600 w-full lg:w-3/12 h-screen">
+          <div className="flex justify-between items-center p-4">
+            <div className="flex items-center gap-2 cursor-pointer">
+              <img src={avatar} className="z-40 h-12" alt="" />
               <h3 className="text-white text-xl">benjamin@sepulveda.com</h3>
             </div>
             <h3 className="text-xl cursor-pointer" onClick={toggleMenu}>
@@ -51,10 +51,10 @@ const HamburgerMenu = () => {
           </div>
           <ul>
             {menuList.map((item, i) => (
-              <li key={i} className="text-center z-20">
+              <li key={i} className="z-20 text-center">
                 <Link
                   to={item.link}
-                  className="block text-white p-4 hover:bg-white hover:text-orange-500 z-20"
+                  className="block z-20 hover:bg-white p-4 text-white hover:text-orange-500"
                   onClick={toggleMenu}
                 >
                   {item.name}
